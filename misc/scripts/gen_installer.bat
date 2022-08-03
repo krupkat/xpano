0438f9a8930046f5efc7c284c9a78896c0394d60
Tomáš Krupka <6817216+krupkat@users.noreply.github.com>
2022-08-03 16:16:43 +0200

    Add an autoinstaller script (#4)
    * Switch to native syntax for expanding environment variables
    
    * Add install target + autogeneration of install scripts
    
    * Add default install prefix
    
    * Make Ubuntu installer script executable
    
    * Simplify install target

---
+++ misc/scripts/gen_installer.bat
@@ -0,0 +1,2 @@
+python ./misc/install/gen_installer.py .github/workflows/ubuntu.yml --output_dir misc/install
+python ./misc/install/gen_installer.py .github/workflows/windows.yml --output_dir misc/install
