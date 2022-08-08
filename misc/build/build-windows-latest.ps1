76ecabcd61abe2ce5c1f5c78e32a92d34bc36001
Tomas Krupka <6817216+krupkat@users.noreply.github.com>
2022-08-08 14:45:19 +0200

    Exclude libjasper from opencv build, sync build script

--- misc/build/build-windows-latest.ps1
+++ misc/build/build-windows-latest.ps1
@@ -5,10 +5,12 @@ $env:SDL_VERSION = 'prerelease-2.23.1'
 $env:OPENCV_VERSION = '4.6.0'
 $env:CATCH_VERSION = 'v3.1.0'
 $env:SPDLOG_VERSION = 'v1.10.0'
+$env:GENERATOR = 'Ninja Multi-Config'
+
 
 git clone https://github.com/opencv/opencv.git --depth 1 --branch $env:OPENCV_VERSION
 cd opencv
-cmake -B build `
+cmake -B build -G "$env:GENERATOR" `
   -DBUILD_SHARED_LIBS=OFF `
   -DCMAKE_INSTALL_PREFIX=install `
   @(Get-Content ../misc/build/opencv_minimal_flags.txt)
@@ -18,7 +20,7 @@ cd ..
 
 git clone https://github.com/libsdl-org/SDL.git --depth 1 --branch $env:SDL_VERSION
 cd sdl
-cmake -B build `
+cmake -B build -G "$env:GENERATOR" `
   -DBUILD_SHARED_LIBS=OFF `
   -DCMAKE_INSTALL_PREFIX=install `
   -DSDL_FORCE_STATIC_VCRT=ON
@@ -28,7 +30,7 @@ cd ..
 
 git clone https://github.com/catchorg/Catch2.git catch --depth 1 --branch $env:CATCH_VERSION
 cd catch
-cmake -B build `
+cmake -B build -G "$env:GENERATOR" `
   -DCMAKE_INSTALL_PREFIX=install `
   -DBUILD_TESTING=OFF `
   -DCMAKE_POLICY_DEFAULT_CMP0091=NEW `
@@ -39,20 +41,20 @@ cd ..
 
 git clone https://github.com/gabime/spdlog.git --depth 1 --branch $env:SPDLOG_VERSION
 cd spdlog
-cmake -B build `
-  -DCMAKE_INSTALL_PREFIX=install_spdlog `
+cmake -B build -G "$env:GENERATOR" `
+  -DCMAKE_INSTALL_PREFIX=build/install `
   -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded
 cmake --build build --target install --config $env:BUILD_TYPE
 cd ..
 
-cmake -B build `
+cmake -B build -G "$env:GENERATOR" `
   -DBUILD_TESTING=ON `
   -DXPANO_STATIC_VCRT=ON `
   -DCMAKE_INSTALL_PREFIX=install `
   -DSDL2_DIR="sdl/install/cmake" `
   -DOpenCV_STATIC=ON `
   -DOpenCV_DIR="opencv/install" `
-  -Dspdlog_DIR="spdlog/install_spdlog/lib/cmake/spdlog" `
+  -Dspdlog_DIR="spdlog/build/install/lib/cmake/spdlog" `
   -DCatch2_DIR="../catch/install/lib/cmake/Catch2" `
 
 cmake --build build --config $env:BUILD_TYPE --target install
