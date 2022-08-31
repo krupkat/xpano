1ad119019cb89990aa75e91d64d335248937cdcc
Tomas Krupka <6817216+krupkat@users.noreply.github.com>
2022-08-31 20:28:58 +0200

    add scripts for building deb packages

---
+++ misc/build/deb-package/package.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+
+if [ -z $1 ]; then
+    echo "You must specify a version."
+    exit 1
+fi
+
+PACKAGE="xpano-$1"
+
+git submodule update --init
+
+# archive xpano
+git archive --prefix "${PACKAGE}/" -o "${PACKAGE}.tar" HEAD
+
+# add submodules
+git submodule foreach --recursive \
+  "git archive --prefix=${PACKAGE}/\$path/ --output=\$sha1.tar HEAD && 
+   tar --concatenate --file=$(pwd)/${PACKAGE}.tar \$sha1.tar && 
+   rm \$sha1.tar"
+
+mkdir packages
+mv "${PACKAGE}.tar" packages && cd packages
+tar xf "${PACKAGE}.tar"
+cd "${PACKAGE}"
+pwd
