341da0324ad4c19ee2bc23b446fb48309db45c75
Tomas Krupka <6817216+krupkat@users.noreply.github.com>
2022-09-06 15:13:08 +0200

    check clang tidy results in the workflow

---
+++ misc/scripts/python/check_fixes.py
@@ -0,0 +1,20 @@
+import argparse
+import yaml
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Check clang-tidy fixes and throw an error if any are found"
+    )
+    parser.add_argument("fixes_path", help="Path to fixes exported from clang-tidy")
+    args = parser.parse_args()
+
+    with open(args.action_path, "r") as file:
+        fixes = yaml.safe_load(file)
+
+    diagnostics = fixes.get("Diagnostics", [])
+    if len(diagnostics) > 0:
+        print("Found warnings in clang-tidy export, please fix them.")
+        exit(1)
+
+    print("No warnings found in clang-tidy export.")
+    exit(0)
