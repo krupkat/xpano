7d080c2a9f39d73bf0e28338f4241b49e55b1fd9
Tomas Krupka <6817216+krupkat@users.noreply.github.com>
2022-09-07 13:40:39 +0200

    add debian packaging files

---
+++ package.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+
+if [ -z $1 ]; then
+    echo "You must specify a version."
+    exit 1
+fi
+
+if [ -z $2 ]; then
+    echo "You must specify a distribution."
+    exit 1
+fi
+
+VERSION=$1
+PACKAGE="xpano-$VERSION"
+DISTRIBUTION=$2
+
+# create and unpack tar.gz in packages/distribution directory
+cd packages
+mkdir -p ${DISTRIBUTION} && cd ${DISTRIBUTION}
+cp "../${PACKAGE}.tar.gz" .
+tar xf "${PACKAGE}.tar.gz"
+cd "${PACKAGE}"
+
+# prepare debian directory
+cp -r misc/build/deb-package/debian .
+jinja -D "dist" $DISTRIBUTION debian/changelog.in -o debian/changelog
+rm debian/changelog.in
+
+# build source package
+debmake
+debuild -S
