bbe8c3a4cc980c36b9f2a145c9dfb2905bbbe5aa
Tomas Krupka <6817216+krupkat@users.noreply.github.com>
2023-04-14 17:18:17 +0200

    update download submodules script

--- misc/scripts/download-submodules.sh
+++ misc/scripts/download-submodules.sh
@@ -3,6 +3,7 @@
 TAG_IMGUI="docking_v1.89.4_wip"
 TAG_NFDE="v1.0.1"
 TAG_THREAD_POOL="v3.3.1"
+TAG_ALPACA="v0.2.1"
 
 cd external
 
@@ -17,5 +18,9 @@ mv nativefiledialog-extended-*/* nativefiledialog-extended
 wget -O thread-pool.tar.gz "https://github.com/krupkat/thread-pool/archive/refs/tags/${TAG_THREAD_POOL}.tar.gz"
 tar xf thread-pool.tar.gz
 mv thread-pool-*/* thread-pool
+
+wget -O alpaca.tar.gz "https://github.com/p-ranav/alpaca/archive/refs/tags/${TAG_ALPACA}.tar.gz"
+tar xf alpaca.tar.gz
+mv alpaca-*/* alpaca
 
 cd ..
