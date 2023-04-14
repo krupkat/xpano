3d8c825d3122fedf3ace037dd7d60b6caea336a8
Tomas Krupka <6817216+krupkat@users.noreply.github.com>
2023-04-14 16:38:14 +0200

    add serialization test

---
+++ tests/serialize_test.cc
@@ -0,0 +1,93 @@
+#include "xpano/utils/serialize.h"
+
+#include <filesystem>
+
+#include <catch2/catch_test_macros.hpp>
+
+#include "tests/utils.h"
+#include "xpano/pipeline/options.h"
+
+struct Bar {
+  int first;
+  int second;
+};
+
+struct BarV2 {
+  int first;
+  int second;
+  int third;
+};
+
+struct Foo {
+  int first;
+  int second;
+  Bar bar;
+};
+
+struct FooV2 {
+  int first;
+  int second;
+  BarV2 bar;
+};
+
+TEST_CASE("Deserialize") {
+  auto tmp_path = xpano::tests::TmpPath();
+  auto foo = Foo{1, 2, {3, 4}};
+
+  xpano::utils::serialize::SerializeWithVersion(tmp_path, foo);
+  auto [status, foo_recovered] =
+      xpano::utils::serialize::DeserializeWithVersion<Foo>(tmp_path);
+
+  REQUIRE(status == xpano::utils::serialize::DeserializeStatus::kSuccess);
+  CHECK(foo_recovered.first == 1);
+  CHECK(foo_recovered.second == 2);
+  CHECK(foo_recovered.bar.first == 3);
+  CHECK(foo_recovered.bar.second == 4);
+  REQUIRE(std::filesystem::remove(tmp_path));
+}
+
+TEST_CASE("Deserialize no such file") {
+  const std::filesystem::path path = "no_such_file";
+
+  auto [status, foo_recovered] =
+      xpano::utils::serialize::DeserializeWithVersion<Foo>(path);
+
+  REQUIRE(status == xpano::utils::serialize::DeserializeStatus::kNoSuchFile);
+}
+
+TEST_CASE("Deserialize breaking change") {
+  auto tmp_path = xpano::tests::TmpPath();
+  auto foo = Foo{1, 2, {3, 4}};
+
+  xpano::utils::serialize::SerializeWithVersion(tmp_path, foo);
+  auto [status, foo_recovered] =
+      xpano::utils::serialize::DeserializeWithVersion<FooV2>(tmp_path);
+
+  REQUIRE(status ==
+          xpano::utils::serialize::DeserializeStatus::kBreakingChange);
+  REQUIRE(std::filesystem::remove(tmp_path));
+}
+
+TEST_CASE("Deserialize pipeline options") {
+  auto tmp_path = xpano::tests::TmpPath();
+  auto options = xpano::pipeline::Options{};
+
+  options.compression.jpeg_quality = 1;
+  options.loading.preview_longer_side = 2;
+  options.matching.neighborhood_search_size = 3;
+  options.stitch.projection.type = xpano::algorithm::ProjectionType::kPanini;
+
+  xpano::utils::serialize::SerializeWithVersion(tmp_path, options);
+  auto [status, options_recovered] =
+      xpano::utils::serialize::DeserializeWithVersion<xpano::pipeline::Options>(
+          tmp_path);
+
+  REQUIRE(status == xpano::utils::serialize::DeserializeStatus::kSuccess);
+  CHECK(options_recovered.compression.jpeg_quality == 1);
+  CHECK(options_recovered.loading.preview_longer_side == 2);
+  CHECK(options_recovered.matching.neighborhood_search_size == 3);
+  CHECK(options_recovered.stitch.projection.type ==
+        xpano::algorithm::ProjectionType::kPanini);
+
+  REQUIRE(std::filesystem::remove(tmp_path));
+}
---
+++ tests/utils.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <filesystem>
+#include <random>
+#include <string>
+
+namespace xpano::tests {
+
+std::filesystem::path TmpPath() {
+  std::string alphanum_chars(
+      "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
+  std::mt19937 generator(std::random_device{}());
+  std::shuffle(alphanum_chars.begin(), alphanum_chars.end(), generator);
+  const int filename_length = 32;
+  return (std::filesystem::temp_directory_path() /
+          alphanum_chars.substr(0, filename_length));
+}
+
+}  // namespace xpano::tests
