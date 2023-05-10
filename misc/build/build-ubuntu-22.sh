7aa481f7715721738843f719ef7c31bd2de64f4b
Tomas Krupka <6817216+krupkat@users.noreply.github.com>
2023-05-10 17:51:45 +0200

    add exiv2 to build scripts

--- misc/build/build-ubuntu-22.sh
+++ misc/build/build-ubuntu-22.sh
@@ -5,6 +5,7 @@ export SDL_VERSION='release-2.26.5'
 export OPENCV_VERSION='4.7.0'
 export CATCH_VERSION='v3.3.2'
 export SPDLOG_VERSION='v1.11.0'
+export EXIV2_VERSION='v0.28.0'
 export GENERATOR='Ninja Multi-Config'
 
 git submodule update --init
@@ -18,12 +19,22 @@ cmake -B build -DCMAKE_INSTALL_PREFIX=install -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DB
 cmake --build build -j $(nproc) --target install
 cd ..
 
+
+git clone https://github.com/Exiv2/exiv2.git --depth 1 --branch $EXIV2_VERSION
+cd exiv2
+cmake -B build \
+  -DCMAKE_INSTALL_PREFIX=install \
+  `cat ../misc/build/exiv2-minimal-flags.txt`
+cmake --build build --target install -j $(nproc)
+cd ..
+
 cmake -B build \
   -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
   -DCMAKE_INSTALL_PREFIX=install \
   -DBUILD_TESTING=ON \
   -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
-  -DCatch2_DIR=../catch/install/lib/cmake/Catch2
+  -DCatch2_DIR=../catch/install/lib/cmake/Catch2 \
+  -Dexiv2_DIR=exiv2/install/lib/cmake/exiv2
 
 cmake --build build -j $(nproc) --target install
 cd build
