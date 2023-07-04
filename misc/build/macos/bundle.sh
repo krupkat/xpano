048441ae54237e0d3c213ac6ff4bde55d26da8bf
Martin Straeten <39386816+MStraeten@users.noreply.github.com>
2023-07-04 11:27:27 +0200

    Create a MacOS bundle (#100)
    * add icns file for macOS package
    
    * create macOS app bundle
    
    * Update bundlescriptXpano
    
    * move macos bundling files to separate folder
    
    * change app identifier to cz.krupkat.Xpano
    
    * add high resolution capability to plist
    
    * reuse variables in macos bundle script
    
    * update comment in macos bundle script
    
    * handling further dependency: libiconv
    
    libexiv2 depends on libiconv
    
    * updated icons based on svg instead of scaled png
    
    * corrected set
    
    * autodetect local and macports dependencies
    
    get rid of hardcoded libraries ;)
    
    * add macos buddle script to CI
    
    * switch to lib install directory in bundle script
    
    those are cached in CI
    
    * add debug prints
    
    * exit bundle script on error
    
    * Revert "exit bundle script on error"
    
    This reverts commit 77dcfe78a410df0077a9dec6430bbed30b7d6911.
    
    * exit bundle script on error 2nd try
    
    ---------
    
    Co-authored-by: Tomas Krupka <6817216+krupkat@users.noreply.github.com>

---
+++ misc/build/macos/bundle.sh
@@ -0,0 +1,110 @@
+#!/bin/sh
+# macports based, should work with brew as well
+
+set -e
+
+appname=Xpano
+appfolder=$appname.app
+macosfolder=$appfolder/Contents/MacOS
+libfolder=$appfolder/Contents/Resources/lib
+plistfile=$appfolder/Contents/Info.plist
+appfile=Xpano
+
+PkgInfoContents="APPL????"
+
+#
+if [ -e $appfolder ]
+then
+  echo "$appfolder already exists, deleting"
+  rm -rf $appfolder
+fi
+
+echo "Creating $appfolder..."
+mkdir -p $macosfolder $libfolder
+
+# Copy App
+cp ./install/bin/$appfile $macosfolder/$appfile
+echo "App copied..."
+# Copy the resource files to the correct place
+cp -r ./install/share $appfolder/Contents/
+echo "share copied..."
+
+#get local dependencies
+rpathdependencies=$(otool -L ./install/bin/Xpano| awk 'NR>1{print $1}' | grep @rpath | sed 's/@rpath\///') || true
+
+echo "rpath dependencies..."
+
+# for debugging
+otool -L ./install/bin/Xpano
+
+for dep in $rpathdependencies; do 
+  case ${dep:0:5} in 
+   'libop' ) 
+   libfile='./opencv/install/lib/'$dep;;
+   'libex' ) 
+   libfile='./exiv2/install/lib/'$dep;;
+  esac
+ echo "handling dep" $dep
+  
+  #copy lib
+  cp $libfile $libfolder
+  #inametool
+  install_name_tool -change @rpath/$dep @executable_path/../Resources/lib/$dep $macosfolder/$appfile
+  
+  # for debugging
+  otool -L $libfile
+
+  #detect and copy second level macports dependencies
+  submacportsdependencies=$(otool -L $libfile | awk 'NR>1{print $1}' | grep opt) || true
+  for subdep in $submacportsdependencies; do 
+   echo "handlingsubdep " $subdep
+   #copy lib
+   cp $subdep $libfolder
+   #inametool
+   install_name_tool -change $subdep @rpath/$(basename $subdep) $libfolder/$(basename $dep)  
+  done 
+  
+done 
+
+#get macports dependencies
+macportsdependencies=$(otool -L ./install/bin/Xpano| awk 'NR>1{print $1}' | grep opt) || true
+
+for dep in $macportsdependencies; do 
+ echo "handling dep" $dep
+ #copy lib
+ cp $dep $libfolder
+ #inametool
+ install_name_tool -change $dep @executable_path/../Resources/lib/$(basename $dep) $macosfolder/$appfile
+done 
+
+#handle icon
+cp ./misc/build/macos/$appfile.icns $appfolder/Contents/Resources/
+
+# Create PkgInfo file.
+  echo $PkgInfoContents >$appfolder/Contents/PkgInfo
+#
+# Create information property list file (Info.plist).
+  echo '<?xml version="1.0" encoding="UTF-8"?>' >$plistfile
+  echo '<!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">' >>$plistfile
+  echo '<plist version="1.0">' >>$plistfile
+  echo '<dict>' >>$plistfile
+  echo '  <key>CFBundleDevelopmentRegion</key>' >>$plistfile
+  echo '  <string>English</string>' >>$plistfile
+  echo '  <key>CFBundleExecutable</key>' >>$plistfile
+  echo '  <string>'$appfile'</string>' >>$plistfile
+  echo '  <key>CFBundleIconFile</key>' >>$plistfile
+  echo '  <string>'$appfile.icns'</string>' >>$plistfile
+  echo '  <key>CFBundleIdentifier</key>' >>$plistfile
+  echo '  <string>cz.krupkat.'$appname'</string>' >>$plistfile
+  echo '  <key>CFBundleInfoDictionaryVersion</key>' >>$plistfile
+  echo '  <string>6.0</string>' >>$plistfile
+  echo '  <key>CFBundlePackageType</key>' >>$plistfile
+  echo '  <string>APPL</string>' >>$plistfile
+  echo '  <key>CFBundleSignature</key>' >>$plistfile
+  echo '  <string>????</string>' >>$plistfile
+  echo '  <key>CFBundleVersion</key>' >>$plistfile
+  echo '  <string>1.0</string>' >>$plistfile
+  echo '  <key>NSHighResolutionCapable</key>' >>$plistfile
+  echo '  <string>True</string>' >>$plistfile
+  echo '</dict>' >>$plistfile
+  echo '</plist>' >>$plistfile
