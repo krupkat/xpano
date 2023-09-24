0c3fd6f795e81987b26d378da2a47104889b1ad9
Tomáš Krupka <6817216+krupkat@users.noreply.github.com>
2023-09-24 15:04:53 +0200

    Feature AppImage (#110)
    * add build script for AppDir
    
    * add linuxdeploy command to bundle appdir to appimage

---
+++ misc/build/build-appimage.sh
@@ -0,0 +1,18 @@
+./misc/build/build-ubuntu-20.sh
+
+mkdir licenses
+
+cp SDL/LICENSE.txt licenses/sdl-license.txt
+cp spdlog/LICENSE licenses/spdlog-license.txt
+cp exiv2/COPYING licenses/exiv2-license.txt
+cp opencv/install/share/licenses/opencv4/* licenses
+cp opencv/LICENSE licenses/opencv-license.txt
+
+cmake -B build \
+  -DCMAKE_INSTALL_PREFIX=/usr \
+  -DXPANO_INSTALL_DEPENDENCIES=ON \
+  -DXPANO_EXTRA_LICENSES=licenses
+
+DESTDIR=AppDir cmake --build build -j $(nproc) --target install
+
+linuxdeploy-x86_64.AppImage --appdir build/AppDir --output appimage
