9fb99e0bb3c03848f502b43427694fba5ec42f24
Tomas Krupka <6817216+krupkat@users.noreply.github.com>
2026-07-29 22:46:17 +0200

    Revert "opencv cleanup"
    This reverts commit c29c7ed39ee85124b0d5d54cd23ffd6be07c6e02.

--- xpano/gui/panels/sidebar.cc
+++ xpano/gui/panels/sidebar.cc
@@ -26,6 +26,7 @@
 #include "xpano/utils/exiv2.h"
 #include "xpano/utils/fmt.h"
 #include "xpano/utils/imgui_.h"
+#include "xpano/utils/opencv.h"
 
 namespace xpano::gui {
 
@@ -137,13 +138,15 @@ void DrawExportOptionsMenu(pipeline::MetadataOptions* metadata_options,
                      kMaxJpegQuality);
     ImGui::Checkbox("Progressive", &compression_options->jpeg_progressive);
     ImGui::Checkbox("Optimize", &compression_options->jpeg_optimize);
-    ImGui::Text("Chroma subsampling:");
-    ImGui::SameLine();
-    utils::imgui::RadioBox(&compression_options->jpeg_subsampling,
-                           pipeline::kSubsamplingModes);
-    utils::imgui::InfoMarker("(?)",
-                             "Corresponding to the 4:4:4, 4:2:2 and 4:2:0 "
-                             "chroma subsampling modes.");
+    if constexpr (utils::opencv::HasJpegSubsamplingSupport()) {
+      ImGui::Text("Chroma subsampling:");
+      ImGui::SameLine();
+      utils::imgui::RadioBox(&compression_options->jpeg_subsampling,
+                             pipeline::kSubsamplingModes);
+      utils::imgui::InfoMarker("(?)",
+                               "Corresponding to the 4:4:4, 4:2:2 and 4:2:0 "
+                               "chroma subsampling modes.");
+    }
     ImGui::Separator();
     ImGui::Text("PNG");
     ImGui::SliderInt("Compression", &compression_options->png_compression, 0,
@@ -460,7 +463,10 @@ cv::Mat DrawMatches(const algorithm::Match& match,
   const auto single_point_color = cv::Scalar::all(-1);
   const auto matches_mask = std::vector<char>();
   cv::drawMatches(img1.GetPreview(), img1.GetKeypoints(), img2.GetPreview(),
-                  img2.GetKeypoints(), match.matches, out, match_thickness,
+                  img2.GetKeypoints(), match.matches, out,
+#if XPANO_OPENCV_HAS_NEW_DRAW_MATCHES_API
+                  match_thickness,
+#endif
                   match_color, single_point_color, matches_mask,
                   cv::DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
   return out;
